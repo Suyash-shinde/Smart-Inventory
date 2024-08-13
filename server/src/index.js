b17e955954f4c7b7da627886b52aff1ab5f4cf56
Suyash Shinde <suyash.c.shinde@gmail.com>
2024-08-13 23:10:26 +0530

    basic server set up

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,13 @@
 import 'dotenv/config'
 import express from 'express';
+import connectDb from './Db/index.js';
+import cors from 'cors';
+import cookieParser from "cookie-parser";
 const app=express();
 const port=process.env.PORT;
-
+app.use(cors());
+app.use(cookieParser())
+connectDb();
 
 app.listen(port, ()=>{
     console.log(`Server is listening on port http://localhost:${port}`);
