52cf5bce93611aa85ebf2aa702febb8cf307827a
Sidh Jain <sidhj211@gmail.com>
2024-08-17 12:11:01 +0530

    dashboard page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
+import Dashboard from './Dashboard/page.js'
 export default function Home() {
   return (
     <>
       <div className="w-screen h-screen bg-red-400"></div>
+      <Dashboard/>
     </>
   );
 }
