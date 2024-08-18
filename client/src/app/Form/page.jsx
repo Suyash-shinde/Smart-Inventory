1452e8f16a3aea721c8778519acc4e4be6e75dc8
Sidh Jain <sidhj211@gmail.com>
2024-08-18 22:42:24 +0530

    Complete form css with state management

--- client/src/app/Form/page.jsx
+++ client/src/app/Form/page.jsx
@@ -1,34 +1,79 @@
 "use client"
 import React,{useState} from 'react'
 import Navbar from '../Components/Navbar'
+
 import styles from './form.css'
 const page = () => {
+    const getDate = () => {
+        const today=new Date();
+        const month=today.getMonth()+1;
+        const year=today.getFullYear();
+        const date=today.getDate();
+        const currentDate = date + "/" +month  + "/" + year;
+        return currentDate;
+    }
+    const date=getDate();
         const [issueDetails,setIssueDetails] = useState({
-
+            deviceId:'',
+            deviceType:'',
+            date:date,
+            facultyName:'',
+            facultyLabIncharge:'',
+            details:''
         });
+        const handleChange = (e) =>{
+                const {name,value} =e.target;
+             setIssueDetails((prevState)=> ({...prevState,[name]:e.target.value}));
+                //  console.log(issueDetails);
+                  
+            };
+    // function handleChangeFacultyName (e) {
+        
+    //     setIssueDetails(e.target.value);
+        
+    //     console.log(e.target.value);
+        
+    // }   
+    
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(issueDetails);
+    
+    //idhar se backend mai data bhejna h /)
+  }; 
+
+  const handleReset = (e) =>{
+    window.alert("You are about to reset");
+  }
+
   return (
     <>
         <Navbar/>
         <div className='min-h-screen bg-green-200 py-2'>
                 <div className='Form'>
                     <form >
+                        <div>
                         <label className='' > Faculty Name: </label>
                         {/* We need to get this automatically from login details */}
-                        <input className='border-2 '  id='FacultyName' placeholder='Faculty Name' type='text'/>
-                        <br></br>
+                        <input className='border-2 '  id='facultyName' name='facultyName' placeholder='Faculty Name' type='text' value={issueDetails.facultyName} onChange={handleChange} />
+                        </div>
                         
+                        <div>
                         <label > Faculty Lab Incharge: </label>
                         {/* We need to get this automatically from either lab ka details or something like that , maybe a hardcoded json of each lab to faculty,login details */}
-                        <input type='text' id='FacultyLab' placeholder='Faculty Lab'/>
-                        <br></br>
+                        <input type='text' name='facultyLabIncharge'id='FacultyLab' placeholder='Faculty Lab' value={issueDetails.facultyLabIncharge} onChange={handleChange}/>
+                        </div>
                         
+                        <div>
                         <label  >Date:</label>
                         {/* Automatically from OS or something */}
-                        <input type='Date' id='Date'></input>
-                        <br></br>
+                        <input type='date' placeholder="dd-mm-yyyy" name='date'  id='Date'  value={issueDetails.date} onChange={handleChange}></input>
+                        </div>
 
+                        <div>
                         <label  > Equipment : </label>
-                            <select name='deviceType' >
+                            <select name='deviceType' value={issueDetails.deviceType} onChange={handleChange}>
                                 <option value=''>Choose an Equipment</option>
                                 <option value="Monitor">Monitor</option>
                                 <option value="PC">PC</option>
@@ -36,21 +81,24 @@ const page = () => {
                                 <option value="Fan">Fan</option>
 
                             </select>
-                        <br></br>
+                        </div>
                         
+                        <div>
                         <label  >Device Id:</label>
                         {/* try to make a drop down select later */}
-                        <input type='number' id='deviceId'></input>
+                        <input type='text' name='deviceId' value={issueDetails.deviceId} onChange={handleChange}></input>
                         
-                        <br></br>
+                        </div>
                         
+                        <div>
                         <label >Issue</label>
-                        <input type='text' id='Issue'></input>
-
-                        <br></br>
+                        <textarea  name='details' value={issueDetails.details} onChange={handleChange}></textarea>
+                        </div>
+                        
                         <div className='bottomButtons'>
-                        <input  className=' rounded-xl'type="reset" value="Clear" />
-                        <input className=' rounded-full' type="submit" value="Send Request" />
+                        <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded mr-[58%] " type='reset'
+                        onClick={handleReset} >Delete </button>
+                        <button className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"  type='submit' onClick={handleSubmit} > Submit</button>
                         </div>
                     </form>
                 </div>
