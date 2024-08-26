074d2d1c7a2e356871ffe5ec5fea2ef447dc7722
Chinmay Wadhe <chinmaywadhe26@gmail.com>
2024-08-26 18:53:39 +0530

    navbar

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
-
+import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <>
-      <div className="w-screen h-screen bg-red-400">
-       
+      <div>
+
+      <Navbar/>
       </div>
     </>
   );
