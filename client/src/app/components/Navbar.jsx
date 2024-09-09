814dd2494d124d953baca31eb1d42d49f8a736e1
Sidh Jain <sidhj211@gmail.com>
2024-09-09 12:15:28 +0530

    add

--- client/src/app/components/Navbar.jsx
+++ client/src/app/components/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
-
+import { ToggleLightMode } from "./ToggleLightMode";
 import { FiMenu } from "react-icons/fi";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { IoCloseOutline } from "react-icons/io5";
@@ -75,6 +75,7 @@ export default function Navbar() {
             ))}
           </section>
         </div>
+        <ToggleLightMode/>
       </nav>
       <hr className=" " />
     </main>
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <>
+    
     <Navbar/>
       <div className="w-screen h-screen bg-red-400"></div>
       
