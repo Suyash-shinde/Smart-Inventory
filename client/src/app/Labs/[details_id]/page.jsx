3cb075d72626fb59b4fbf2398fdf3cf7f675a5f6
Chinmay Wadhe <chinmaywadhe26@gmail.com>
2024-09-09 19:59:14 +0530

    labdetails problem solved

--- client/src/app/Labs/[details_id]/page.jsx
+++ client/src/app/Labs/[details_id]/page.jsx
@@ -8,11 +8,21 @@ import { cards } from "../../data";
 const LabDetails = ({ params }) => {
   const id = params.details_id;
   console.log(id);
+  const [lab, setLab] = useState({
+    labName: "",
+    labDescription: "",
+    labIncharge: "",
+  });
   useEffect(() => {
     if (id) {
       const card = cards.find((card) => card.index === parseInt(id));
       if (card) {
         console.log(card);
+        setLab({
+          labName: card.labName,
+          labDescription: card.labDescription,
+          labIncharge: card.labIncharge,
+        });
       } else {
         console.error("Card not found");
       }
@@ -22,17 +32,14 @@ const LabDetails = ({ params }) => {
   return (
     <>
       {" "}
-      {/* <div>lab details of lab {id}</div>
+      <div>lab details of lab {id}</div>
       <div className="min-h-screen p-6 bg-slate-100">
         <div className="flex flex-col items-center p-10 bg-white border-2 shadow-lg rounded-xl">
-          <h1 className="text-4xl font-bold text-gray-800">
-            {selectedLab.labName}
-          </h1>
-          <p className="text-lg mt-4">{selectedLab.labDescription}</p>
-          <p className="text-lg mt-4">Incharge: {selectedLab.labIncharge}</p>
+          <h1 className="text-4xl font-bold text-gray-800">{lab.labName}</h1>
+          <p className="text-lg mt-4">{lab.labDescription}</p>
+          <p className="text-lg mt-4">Incharge: {lab.labIncharge}</p>
         </div>
-      </div> */}
-      <div>hi</div>
+      </div>
     </>
   );
 };
