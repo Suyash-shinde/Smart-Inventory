566f00325e09eaf53ba24c3fd6347c9800a3dfc5
Suyash Shinde <suyash.c.shinde@gmail.com>
2024-09-24 23:33:23 +0530

    added create lab controller

---
+++ server/src/Controllers/lab.controller.js
@@ -0,0 +1,27 @@
+import { Device } from "../Models/device.model";
+
+export const addlab=async(req,res,next)=>{
+    try{
+        const {devices,labNo,row,column,incharge}=req.body;
+        const addDevice = await Device.insertMany(devices);
+        if(!addDevice){
+            return res.json({
+                msg:"Error updating devices",
+                status:false,
+            })
+        }
+        const addLab = await Lab.insertOne({labNo,row,column,incharge,devices});
+        if(!addLab){
+            return res.json({
+                msg:"Error creating Lab",
+                status:false,
+            })
+        }
+        return res.json({
+            msg:"New Lab/ClassRoom Created",
+            status:true,
+        })
+    }catch(error){
+        next(error);
+    }
+}
