b0a35bde39d59bd91dc2b0933eeb53a6022a33ac
Egon15 <pradnesh.tilekar_comp22@pccoer.in>
2024-10-04 11:00:56 +0530

    PDF Form change

--- client/src/app/Labs/[details_id]/Form/Pdf/formIssue.jsx
+++ client/src/app/Labs/[details_id]/Form/Pdf/formIssue.jsx
@@ -1,47 +1,56 @@
-import React, {useState, useEffect,forwardRef } from 'react';
-import { formatToUKDate,getDate } from '@/app/components/date';
-import axios from 'axios';
+import React, { useState, useEffect, forwardRef } from "react";
+import { formatToUKDate, getDate } from "@/app/components/date";
+import axios from "axios";
 
-const FormIssue = forwardRef(({location,deviceId}, ref) => {
-  let date=getDate();
-  date=formatToUKDate(date);
+const FormIssue = forwardRef(({ location, deviceId }, ref) => {
+  let date = getDate();
+  date = formatToUKDate(date);
   const today = new Date();
-    const year = today.getFullYear();
-    const [issueData,setIssueData]=useState({});
-    useEffect(() => {
-      axios.post('http://localhost:3090/api/issues/getIssue', {
-        id: deviceId
+  const year = today.getFullYear();
+  const [issueData, setIssueData] = useState({});
+  useEffect(() => {
+    axios
+      .post("http://localhost:3090/api/issues/getIssue", {
+        id: deviceId,
       })
-        .then((response) => {
-          console.log(response.data.data);
-          const data = response.data.data;
-          const calculatedDept = data.labNo ? getDepartment(data.labNo) : ""; // Handle missing labNo
-          setIssueData({ ...data, department: calculatedDept }); // Update data with department
-        })
-        .catch((err) => {
-          console.error("Error Fetching Data", err);
-        });
-    }, [deviceId]);
-    
-    // Function to determine department based on labNo
-    const getDepartment = (labNo) => {
-      let dept = "";
-      const no = ((labNo - labNo % 100) / 100);
-      if (no === 1) dept = "Civil";
-      if (no === 2) dept = "Mechanical";
-      if (no === 3) dept = "First Year";
-      if (no === 4) dept = "EnTC";
-      if (no === 5) dept = "Computer";
-      return dept;
-    }; //write controller and stuff to get lab details from a particular lab number and id
+      .then((response) => {
+        console.log(response.data.data);
+        const data = response.data.data;
+        const calculatedDept = data.labNo ? getDepartment(data.labNo) : ""; // Handle missing labNo
+        setIssueData({ ...data, department: calculatedDept }); // Update data with department
+      })
+      .catch((err) => {
+        console.error("Error Fetching Data", err);
+      });
+  }, [deviceId]);
+
+  // Function to determine department based on labNo
+  const getDepartment = (labNo) => {
+    let dept = "";
+    const no = (labNo - (labNo % 100)) / 100;
+    if (no === 1) dept = "Civil";
+    if (no === 2) dept = "Mechanical";
+    if (no === 3) dept = "First Year";
+    if (no === 4) dept = "EnTC";
+    if (no === 5) dept = "Computer";
+    return dept;
+  }; //write controller and stuff to get lab details from a particular lab number and id
   return (
-    <div ref={ref} className="w-screen text-xs font-semibold p-8" style={{ height: '297mm', overflowY: 'auto' }}>
+    <div
+      ref={ref}
+      className="w-screen p-8 text-xs font-semibold"
+      style={{ height: "297mm", overflowY: "auto" }}
+    >
       <div>
         <div className="grid grid-cols-6 grid-rows-3 font-bold">
-          <div className="col-span-1 row-span-3 content-center border-1 border-black p-2 text-center">
-          <img src="/image.png" alt="College logo" className=" h-auto w-auto" />
+          <div className="content-center col-span-1 row-span-3 p-2 text-center border-black border-1">
+            <img
+              src="/image.png"
+              alt="College logo"
+              className="w-auto h-auto "
+            />
           </div>
-          <div className="col-span-4 row-span-3 content-center border-1 border-black p-2 text-center text-xl">
+          <div className="content-center col-span-4 row-span-3 p-2 text-xl text-center border-black border-1">
             <p>Pimpri Chinchwad Education Trust's</p>
             <p>
               Pimpri Chinchwad College of Engineering & Research <br />
@@ -49,82 +58,129 @@ const FormIssue = forwardRef(({location,deviceId}, ref) => {
             </p>
             <p>IQAC PCCOER</p>
           </div>
-          <div className="col-span-1 row-span-3 content-center border-1 border-black p-2 text-center">
-          <img src="/pcet.png" alt="College logo" className=" h-auto w-auto" />
+          <div className="content-center col-span-1 row-span-3 p-2 text-center border-black border-1">
+            <img
+              src="/pcet.png"
+              alt="College logo"
+              className="w-auto h-auto "
+            />
           </div>
-          <div className="col-span-1 row-span-1 content-center border-1 border-black p-2 text-center">
-            A. Year: {year}-{year+1} <br />
+          <div className="content-center col-span-1 row-span-1 p-2 text-center border-black border-1">
+            A. Year: {year}-{year + 1} <br />
             Term: I/II
           </div>
-          <div className="col-span-4 row-span-1 content-center border-1 border-black p-2 text-center">Maintenance Report</div>
-          <div className="col-span-1 row-span-1 content-center border-1 border-black p-2 text-center">IT/R/03</div>
+          <div className="content-center col-span-4 row-span-1 p-2 text-center border-black border-1">
+            Maintenance Report
+          </div>
+          <div className="content-center col-span-1 row-span-1 p-2 text-center border-black border-1">
+            IT/R/03
+          </div>
         </div>
         <div className="grid grid-cols-7 grid-rows-1 py-3">
-          <div className="col-span-5 row-span-1 content-center p-2">Type of Problem: System/Furniture/Civil/Electrical/Workshop</div>
-          <div className="col-span-2 row-span-1 content-center p-2">Date: {date} </div>
+          <div className="content-center col-span-5 row-span-1 p-2">
+            Type of Problem: System/Furniture/Civil/Electrical/Workshop
+          </div>
+          <div className="content-center col-span-2 row-span-1 p-2">
+            Date: {date}{" "}
+          </div>
         </div>
       </div>
-      <div className="grid h-full w-full grid-cols-6 grid-rows-9 gap-y-3">
-        <div className="col-span-6 row-span-2 grid grid-cols-7 grid-rows-8 border-1 border-black">
-          <div className="p-2-2 col-span-1 row-span-8 content-center border-1 border-black p-2 text-center">Originator</div>
-          <div className="col-span-3 row-span-1 border-1 border-black p-2">Department: {issueData.department}</div>
-          <div className="col-span-3 row-span-1 border-1 border-black p-2">Location:{location}</div>
-          <div className="col-span-6 row-span-3 border-1 border-black p-2">Complaint Details:
-            <p className='font-mono'>
-              {issueData.details}
-            </p>
+      <div className="grid w-full h-full grid-cols-6 grid-rows-9 gap-y-3">
+        <div className="grid grid-cols-7 col-span-6 row-span-2 border-black grid-rows-8 border-1">
+          <div className="content-center col-span-1 p-2 text-center border-black p-2-2 row-span-8 border-1">
+            Originator
           </div>
-          <div className="col-span-3 row-span-1 border-1 border-black p-2">Recuring Complaint (Yes/No):
-            
-              {issueData.recurring?"Yes":"No"}
-            
+          <div className="col-span-3 row-span-1 p-2 border-black border-1">
+            Department: {issueData.department}
           </div>
-          <div className="col-span-3 row-span-1 border-1 border-black p-2">If Yes, how many times:</div>
-          <div className="col-span-3 row-span-3 content-end border-1 border-black p-2 text-center">
-          {issueData.facultyLabIncharge}
-            <br/>
+          <div className="col-span-3 row-span-1 p-2 border-black border-1">
+            Location:{location}
+          </div>
+          <div className="col-span-6 row-span-3 p-2 border-black border-1">
+            Complaint Details:
+            <p className="font-mono">{issueData.details}</p>
+          </div>
+          <div className="col-span-3 row-span-1 p-2 border-black border-1">
+            Recuring Complaint (Yes/No):
+            {issueData.recurring ? "Yes" : "No"}
+          </div>
+          <div className="col-span-3 row-span-1 p-2 border-black border-1">
+            If Yes, how many times:
+          </div>
+          <div className="content-end col-span-3 row-span-3 p-2 text-center border-black border-1">
+            {issueData.facultyLabIncharge}
+            <br />
             Incharge/Lab Assistant
-             <br />Name and Signature with Date</div>
-          <div className="col-span-3 row-span-3 content-end border-1 border-black p-2 text-center">
+            <br />
+            Name and Signature with Date
+          </div>
+          <div className="content-end col-span-3 row-span-3 p-2 text-center border-black border-1">
             Head of Department <br />
             Signature with Date
           </div>
         </div>
-        <div className="col-span-6 row-span-1 grid grid-cols-7 grid-rows-3 border-1 border-black">
-  <div className="col-span-1 row-span-3 content-center border-1 border-black p-1 text-center">Verification</div>
-  <div className="col-span-3 row-span-2 content-center border-1 border-black p-1 text-center">
-    Maintenance Section Incharge <br />
-    Complaint received date with signature
-  </div>
-  <div className="col-span-3 row-span-2 border-1 border-black p-1">
-    Name of the person to whom work is allocated:
-  </div>
-  <div className="col-span-3 row-span-1 border-1 border-black p-1">
-    Verification and Remarks:
-  </div>
-  <div className="col-span-3 row-span-1 border-1 border-black p-1">
-    Material replaced/repaired/used for attending complaint:
-  </div>
-</div>
+        <div className="grid grid-cols-7 col-span-6 grid-rows-3 row-span-1 border-black border-1">
+          <div className="content-center col-span-1 row-span-3 p-1 text-center border-black border-1">
+            Verification
+          </div>
+          <div className="content-end col-span-3 row-span-2 p-1 text-center border-black border-1">
+            Maintenance Section Incharge <br />
+            Complaint received date with signature
+          </div>
+          <div className="col-span-3 row-span-2 p-1 border-black border-1">
+            Name of the person to whom work is allocated:
+          </div>
+          <div className="col-span-3 row-span-1 p-1 border-black border-1">
+            Verification and Remarks:
+          </div>
+          <div className="col-span-3 row-span-1 p-1 border-black border-1">
+            Material replaced/repaired/used for attending complaint:
+          </div>
+        </div>
 
-        <div className="col-span-6 row-span-2 grid grid-cols-7 grid-rows-6 border-1 border-black">
-          <div className="p-2-2 col-span-1 row-span-6 content-center border-1 border-black p-2 text-center">Correction Action</div>
-          <div className="col-span-3 row-span-2 border-1 border-black p-2">Complaint resolved and closed inhouse</div>
-          <div className="col-span-3 row-span-2 border-1 border-black p-2">Remark:</div>
-          <div className="col-span-3 row-span-2 border-1 border-black p-2">Purchase of consumable required recommended</div>
-          <div className="col-span-3 row-span-2 border-1 border-black p-2">Details (Description/Oty/Cost):</div>
-          <div className="col-span-3 row-span-2 border-1 border-black p-2">Reommended Maintenance from external agency</div>
-          <div className="p-2-2 col-span-3 row-span-2 content-center space-y-3 border-1 border-black p-2">
+        <div className="grid grid-cols-7 col-span-6 grid-rows-6 row-span-2 border-black border-1">
+          <div className="content-center col-span-1 row-span-6 p-2 text-center border-black p-2-2 border-1">
+            Correction Action
+          </div>
+          <div className="col-span-3 row-span-2 p-2 border-black border-1">
+            Complaint resolved and closed inhouse
+          </div>
+          <div className="col-span-3 row-span-2 p-2 border-black border-1">
+            Remark:
+          </div>
+          <div className="col-span-3 row-span-2 p-2 border-black border-1">
+            Purchase of consumable required recommended
+          </div>
+          <div className="col-span-3 row-span-2 p-2 border-black border-1">
+            Details (Description/Oty/Cost):
+          </div>
+          <div className="col-span-3 row-span-2 p-2 border-black border-1">
+            Reommended Maintenance from external agency
+          </div>
+          <div className="content-center col-span-3 row-span-2 p-2 space-y-3 border-black p-2-2 border-1">
             <p>Agency name:</p>
             <p>Expected Expenditure (Approx):</p>
           </div>
         </div>
-        <div className="col-span-6 row-span-2 grid grid-cols-7 grid-rows-4 border-1 border-black">
-          <div className="p-2-2 col-span-1 row-span-4 content-center border-1 border-black p-2 text-center">Maintenance Report Closure</div>
-          <div className="col-span-3 row-span-2 border-1 border-black p-2">Remark on work completion by person incharge of the lab/className:</div>
-          <div className="col-span-3 row-span-2 content-end border-1 border-black p-2 text-center">Incharge/Lab Assistant <br />Name and Signature with Date</div>
-          <div className="col-span-3 row-span-2 border-1 border-black p-2">Remark on work completion by Maintenance section Incharge:</div>
-          <div className="col-span-3 row-span-2 content-end border-1 border-black p-2 text-center">Maintenance Section Incharge<br />Complaint closed Date with Signature</div>
+        <div className="grid grid-cols-7 col-span-6 grid-rows-4 row-span-2 border-black border-1">
+          <div className="content-center col-span-1 row-span-4 p-2 text-center border-black p-2-2 border-1">
+            Maintenance Report Closure
+          </div>
+          <div className="col-span-3 row-span-2 p-2 border-black border-1">
+            Remark on work completion by person incharge of the lab/className:
+          </div>
+          <div className="content-end col-span-3 row-span-2 p-2 text-center border-black border-1">
+            Incharge/Lab Assistant <br />
+            Name and Signature with Date
+          </div>
+          <div className="col-span-3 row-span-2 p-2 border-black border-1">
+            Remark on work completion by Maintenance section Incharge:
+          </div>
+          <div className="content-end col-span-3 row-span-2 p-2 text-center border-black border-1">
+            Maintenance Section Incharge
+            <br />
+            Complaint closed Date with Signature
+          </div>
         </div>
       </div>
     </div>
--- client/src/app/data.js
+++ client/src/app/data.js
@@ -1,128 +1,128 @@
 export const cards = [
-    {
-      index: 1,
-      labName: "Lab 1",
-      labImage: "https://picsum.photos/200/300",
-      labDescription: "This is lab 1",
-      labIncharge: "John Doe",
-    },
-    {
-      index: 2,
-      labName: "Lab 2",
-      labImage: "https://picsum.photos/200/300",
-      labDescription: "This is lab 2",
-      labIncharge: "Jane Doe",
-    },
-    {
-      index: 3,
-      labName: "Lab 3",
-      labImage: "https://picsum.photos/200/300",
-      labDescription: "This is lab 3",
-      labIncharge: "John wick",
-    },
-    {
-      index: 4,
-      labName: "Lab 4",
-      labImage: "https://picsum.photos/200/300",
-      labDescription: "This is lab 4",
-      labIncharge: "Jane Doe",
-    },
-    {
-      index: 5,
-      labName: "Lab 5",
-      labImage: "https://picsum.photos/200/300",
-      labDescription: "This is lab 5",
-      labIncharge: "John Doe",
-    },
-    {
-      index: 6,
-      labName: "Lab 6",
-      labImage: "https://picsum.photos/200/300",
-      labDescription: "This is lab 6",
-      labIncharge: "Jane bs",
-    },
-  ];
-  
-export   const classrooms = [
-    {
-      index: 1,
-      classroomName: "Classroom 1",
-      classroomImage: "https://picsum.photos/200/300",
-      classroomDescription: "This is classroom 1",
-      classroomIncharge: "John Doe",
-    },
-    {
-      index: 2,
-      classroomName: "Classroom 2",
-      classroomImage: "https://picsum.photos/200/300",
-      classroomDescription: "This is classroom 2",
-      classroomIncharge: "Jane Doe",
-    },
-    {
-      index: 3,
-      classroomName: "Classroom 3",
-      classroomImage: "https://picsum.photos/200/300",
-      classroomDescription: "This is classroom 3",
-      classroomIncharge: "John Doe",
-    },
-    {
-      index: 4,
-      classroomName: "Classroom 4",
-      classroomImage: "https://picsum.photos/200/300",
-      classroomDescription: "This is classroom 4",
-      classroomIncharge: "Jane Doe",
-    },
-    {
-      index: 5,
-      classroomName: "Classroom 5",
-      classroomImage: "https://picsum.photos/200/300",
-      classroomDescription: "This is classroom 5",
-      classroomIncharge: "John Doe",
-    },
-    {
-      index: 6,
-      classroomName: "Classroom 6",
-      classroomImage: "https://picsum.photos/200/300",
-      classroomDescription: "This is classroom 6",
-      classroomIncharge: "Jane Doe",
-    },
-  ];
-  
-export  const staffrooms = [
-    {
-      index: 1,
-      staffroomName: "Staffroom 1",
-      staffroomImage: "https://picsum.photos/200/300",
-      staffroomDescription: "This is staffroom 1",
-    },
-    {
-      index: 2,
-      staffroomName: "Staffroom 2",
-      staffroomImage: "https://picsum.photos/200/300",
-      staffroomDescription: "This is staffroom 2",
-    },
-    {
-      index: 3,
-      staffroomName: "Staffroom 3",
-      staffroomImage: "https://picsum.photos/200/300",
-      staffroomDescription: "This is staffroom 3",
-    },
-    {
-      index: 4,
-      staffroomName: "Staffroom 4",
-      staffroomImage: "https://picsum.photos/200/300",
-      staffroomDescription: "This is staffroom 4",
-    },
-    {
-      index: 5,
-      staffroomName: "Staffroom 5",
-      staffroomImage: "https://picsum.photos/200/300",
-      staffroomDescription: "This is staffroom 5",
-    },
-    {
-      index: 6,
-      staffroomName: "Staffroom 6",
-      staffroomImage: "https://picsum.photos/200/300",
-      staffroomDescription: "This is staffroom 6",
-    },
-  ];
+  {
+    index: 1,
+    labName: "Lab 1",
+    labImage: "/lab1.jpeg",
+    labDescription: "This is lab 1",
+    labIncharge: "John Doe",
+  },
+  {
+    index: 2,
+    labName: "Lab 2",
+    labImage: "/lab1.jpeg",
+    labDescription: "This is lab 2",
+    labIncharge: "Jane Doe",
+  },
+  {
+    index: 3,
+    labName: "Lab 3",
+    labImage: "/lab1.jpeg",
+    labDescription: "This is lab 3",
+    labIncharge: "John wick",
+  },
+  {
+    index: 4,
+    labName: "Lab 4",
+    labImage: "/lab1.jpeg",
+    labDescription: "This is lab 4",
+    labIncharge: "Jane Doe",
+  },
+  {
+    index: 5,
+    labName: "Lab 5",
+    labImage: "/lab1.jpeg",
+    labDescription: "This is lab 5",
+    labIncharge: "John Doe",
+  },
+  {
+    index: 6,
+    labName: "Lab 6",
+    labImage: "/lab1.jpeg",
+    labDescription: "This is lab 6",
+    labIncharge: "Jane bs",
+  },
+];
+
+export const classrooms = [
+  {
+    index: 1,
+    classroomName: "Classroom 1",
+    classroomImage: "/lab1.jpeg",
+    classroomDescription: "This is classroom 1",
+    classroomIncharge: "John Doe",
+  },
+  {
+    index: 2,
+    classroomName: "Classroom 2",
+    classroomImage: "/lab1.jpeg",
+    classroomDescription: "This is classroom 2",
+    classroomIncharge: "Jane Doe",
+  },
+  {
+    index: 3,
+    classroomName: "Classroom 3",
+    classroomImage: "/lab1.jpeg",
+    classroomDescription: "This is classroom 3",
+    classroomIncharge: "John Doe",
+  },
+  {
+    index: 4,
+    classroomName: "Classroom 4",
+    classroomImage: "/lab1.jpeg",
+    classroomDescription: "This is classroom 4",
+    classroomIncharge: "Jane Doe",
+  },
+  {
+    index: 5,
+    classroomName: "Classroom 5",
+    classroomImage: "/lab1.jpeg",
+    classroomDescription: "This is classroom 5",
+    classroomIncharge: "John Doe",
+  },
+  {
+    index: 6,
+    classroomName: "Classroom 6",
+    classroomImage: "/lab1.jpeg",
+    classroomDescription: "This is classroom 6",
+    classroomIncharge: "Jane Doe",
+  },
+];
+
+export const staffrooms = [
+  {
+    index: 1,
+    staffroomName: "Staffroom 1",
+    staffroomImage: "/lab1.jpeg",
+    staffroomDescription: "This is staffroom 1",
+  },
+  {
+    index: 2,
+    staffroomName: "Staffroom 2",
+    staffroomImage: "/lab1.jpeg",
+    staffroomDescription: "This is staffroom 2",
+  },
+  {
+    index: 3,
+    staffroomName: "Staffroom 3",
+    staffroomImage: "/lab1.jpeg",
+    staffroomDescription: "This is staffroom 3",
+  },
+  {
+    index: 4,
+    staffroomName: "Staffroom 4",
+    staffroomImage: "/lab1.jpeg",
+    staffroomDescription: "This is staffroom 4",
+  },
+  {
+    index: 5,
+    staffroomName: "Staffroom 5",
+    staffroomImage: "/lab1.jpeg",
+    staffroomDescription: "This is staffroom 5",
+  },
+  {
+    index: 6,
+    staffroomName: "Staffroom 6",
+    staffroomImage: "/lab1.jpeg",
+    staffroomDescription: "This is staffroom 6",
+  },
+];
