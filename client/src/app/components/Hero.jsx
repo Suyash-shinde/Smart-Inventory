a9d7a6dc61608bc1b8b7da72fb36f35eb2c34af4
Chinmay Wadhe <chinmaywadhe26@gmail.com>
2024-10-04 09:05:46 +0530

    Landing page

---
+++ client/src/app/components/Hero.jsx
@@ -0,0 +1,13 @@
+import { Particles } from "./particles";
+export const Hero = () => {
+  return (
+    <>
+  
+      <Particles className="absolute inset-0 z-10" />
+      <h1 className=" mt-4 pb-4 font-extrabold tracking-tight text-transparent text-7xl lg:text-8xl  bg-clip-text bg-gradient-to-r from-cyan-400 via-blue-600 to-cyan-400">
+        Smart Inventory
+      </h1>
+     
+    </>
+  );
+};
