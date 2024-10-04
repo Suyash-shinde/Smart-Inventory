fe871c489eb0fdbfde58fa2aa12cb22028024c6c
Chinmay Wadhe <chinmaywadhe26@gmail.com>
2024-10-04 10:37:03 +0530

    landing page buttons working

--- client/src/app/components/Hero.jsx
+++ client/src/app/components/Hero.jsx
@@ -4,7 +4,7 @@ export const Hero = () => {
     <>
   
       <Particles className="absolute inset-0 z-10" />
-      <h1 className="z-50 mt-4 pb-4 font-extrabold tracking-tight text-transparent text-7xl lg:text-8xl  bg-clip-text bg-gradient-to-r from-cyan-400 via-blue-600 to-cyan-400">
+      <h1 className=" mt-4 pb-4 font-extrabold tracking-tight text-transparent text-7xl lg:text-8xl  bg-clip-text bg-gradient-to-r from-cyan-400 via-blue-600 to-cyan-400">
         Smart Inventory
       </h1>
      
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
       <div className="py-20 text-3xl font-bold text-center text-white">
         Identify Your Role
       </div>
-      <div className="flex items-center w-full justify-evenly">
+      <div className="z-50 flex items-center w-full justify-evenly">
         <div
           onClick={handleAdminClick}
           className="flex flex-col items-center justify-center w-56 h-56 text-center transition-colors duration-500 bg-white border shadow-md cursor-pointer group text-slate-800 hover:text-white border-slate-100 rounded-xl hover:bg-emerald-800"
@@ -34,7 +34,7 @@ export default function Home() {
 
         <div
           onClick={handleTechnicianClick}
-          className="flex flex-col items-center justify-center w-56 h-56 text-center transition-colors duration-500 bg-white border shadow-md cursor-pointer group text-slate-800 hover:text-white border-slate-100 rounded-xl hover:bg-sky-800"
+          className=" flex flex-col items-center justify-center w-56 h-56 text-center transition-colors duration-500 bg-white border shadow-md cursor-pointer group text-slate-800 hover:text-white border-slate-100 rounded-xl hover:bg-sky-800"
         >
           <FaUserGear
             size={100}
